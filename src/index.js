d6ad9d353297d78132e7e14d8394437b89929cd7
singhashwin <singhashwin334@gmail.com>
2022-06-15 20:18:41 +0530

    update 2

--- src/index.js
+++ src/index.js
@@ -6,46 +6,46 @@ const userRouter = require('./routers/users')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 27017
+const port = process.env.PORT
  
 const multer = require('multer')
 
-const upload = multer({
-    dest:'images',
-    limit:1000000,
-    fileFilter(req,file,cb){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a word document'))
-        }
-        cb(undefined,true)
-    }
-})
-
-const errorMiddleware = (req,res,next) =>{
-    throw new Error('From my middleware')
-} 
-
-app.post('/upload',upload.single('upload'),(req,res) => {
-    res.send();
-}, (err,req,res,next) => {
-    res.status(400).send()
-})
-
-// app.use((req,res,next) => {
-//     if(req.method == 'GET'){
-//         res.send('Request methods are disabled')
+// const upload = multer({
+//     dest:'images',
+//     limit:1000000,
+//     fileFilter(req,file,cb){
+//         if(!file.originalname.match(/\.(doc|docx)$/)){
+//             return cb(new Error('Please upload a word document'))
+//         }
+//         cb(undefined,true)
 //     }
-//     else{
-//         next()
-//     }
-//     ;
 // })
 
-//site is at maontaenence maode
+// const errorMiddleware = (req,res,next) =>{
+//     throw new Error('From my middleware')
+// } 
 
-// app.use((req,res,next) => {
-//     res.status(503).send('Site is at maintanence mode...Try again later')
-//  })
+// app.post('/upload',upload.single('upload'),(req,res) => {
+//     res.send();
+// }, (err,req,res,next) => {
+//     res.status(400).send()
+// })
+
+// // app.use((req,res,next) => {
+// //     if(req.method == 'GET'){
+// //         res.send('Request methods are disabled')
+// //     }
+// //     else{
+// //         next()
+// //     }
+// //     ;
+// // })
+
+// //site is at maontaenence maode
+
+// // app.use((req,res,next) => {
+// //     res.status(503).send('Site is at maintanence mode...Try again later')
+// //  })
 
 app.use(express.json())
 
@@ -58,6 +58,4 @@ app.listen(port,() => {
 console.log('Server is up on the port '+port);
 }) 
 
-const Task = require('./models/task')
-const User = require('./models/user')
 
